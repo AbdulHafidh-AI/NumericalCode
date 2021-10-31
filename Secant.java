4e3395e5d1a8ede59fcfbcc5064a83ab5138847a
Bravehard700 <Abdul>
2021-11-01 06:57:35 +0700

    Update

--- Secant.java
+++ Secant.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
  * 2.  fungsi f(x) yaitu cos(x+1)+exp(-x+2) dengan nilai x=[1,3], nilai iterasi adalah 10 dan nilai toleransi error adalah 0.001.
  * 3.  fungsi f(x) yaitu cos(x)-sin(x) dengan nilai x=[0,2], nilai iterasi adalah 10 dan nilai toleransi error adalah 0.001.
  * @author Abdul Hafidh
- * @version 2021.10.28
+ * @version 2021.10.30
  */
 public class Secant{
 
@@ -14,7 +14,8 @@ public class Secant{
      * @return x *Math.exp(-x) + Math.toRadians(2 * x)
      */
     public static double fungsiNo1(double x){
-        return x *Math.exp(-x) + Math.toRadians(2 * x);
+        double a = Math.toRadians(2 * x); // Mengubah persamaannya ke nilai radiant dulu sebelum memanggil method sin
+        return x *Math.exp(-x) + Math.sin(a);
     }
 
     /** 
@@ -23,9 +24,9 @@ public class Secant{
      * @return Math.cos(Sudut) + Math.exp(-x + 2)
      */
     public static double fungsiNo2(double x){
-        // Mencari nilai sin pada nilai cos(x + 1)
-        double Sudut = Math.toRadians(x + 1);
-        return Math.cos(Sudut) + Math.exp(-x + 2);
+         // Mengubah ke persamaaan radiant terlebih dahulu
+        double a = Math.toRadians(x + 1);
+        return Math.cos(a) + Math.exp(-x + 2);
     }
 
     /** 
@@ -34,10 +35,10 @@ public class Secant{
      * @return Math.cos(sudut) - sudut2
      */
     public static double fungsiNo3(double x){
-        // Mencari nilai sin untuk cos (x) dan - sin(x)
-        double sudut = Math.toRadians(x);
-        double sudut2 = Math.toRadians(x);
-        return Math.cos(sudut) - sudut2;
+        // Mengubah ke persamaaan radiant terlebih dahulu
+        double sudutCos = Math.toRadians(x); 
+        double sudutSin = Math.toRadians(x);
+        return Math.cos(sudutCos) - Math.sin(sudutSin);
     }
 
     /**
@@ -47,22 +48,42 @@ public class Secant{
     public static void main(String[] args) {
         //Inisialisasi Variabel
         double x0,x1,e;
-        int N;
+        double x2;
+        double fx0,fx1,fx2 = 0;
+        int N,iterasi;
+        String ulang;
+        char lagi;
         int soal;
         Scanner in = new Scanner(System.in);
+        do{
         System.out.print("Masukkan Jawaban soal yang kamu inginkan: ");
         soal = in.nextInt();
         switch (soal) {
             case 1:
-        
+            System.out.println("========================== SOAL NO 1 =======================================");
+            System.out.printf("\nIterasi\t\tx0\t\tf(x0)\t\tx1\t\tf(x1)\t\tx2\t\tf(x2)\n");
+            // Memberi nilai untuk masing-masing variabel di bawah sesuai dengan apa yang diinginkan oleh nomor 1
                 break;
             case 2:
-
+            System.out.println("========================== SOAL NO 2 =======================================");
+            System.out.printf("\nIterasi\t\tx0\t\tf(x0)\t\tx1\t\tf(x1)\t\tx2\t\tf(x2)\n");
+            // Memberi nilai untuk masing-masing variabel di bawah sesuai dengan apa yang diinginkan oleh nomor 2
                  break;
             case 3:
+            System.out.println("========================== SOAL NO 3 =======================================");
+            System.out.printf("\nIterasi\t\tx0\t\tf(x0)\t\tx1\t\tf(x1)\t\tx2\t\tf(x2)\n");
+            // Memberi nilai untuk masing-masing variabel di bawah sesuai dengan apa yang diinginkan oleh nomor 3
                  break;
             default:
                 break;
         }
+         // Membuat suatu pernyataan untuk bermain kembali atau tidak  
+         System.out.print("Apakah kamu ingin melihat jawaban dari soal yang lainya? (y/n) :  ");
+         in.nextLine(); // Memanggil method ini agar bisa dideteksi oleh method Scanner in dari library import java.util.Scanner
+         ulang = in.nextLine();
+         lagi = ulang.charAt(0); // Memanggil method charAt(0) karena pada akhir inputan user hanya boleh menginput satu huruf 
+    }while(lagi == 'Y' || lagi == 'y');
+        // Sebuah Method untuk menutup inputan
+        in.close();
     }
 }
